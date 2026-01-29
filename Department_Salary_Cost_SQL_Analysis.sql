e5719434507165e9416ac5ccceac6524a1d40244
Cardona-SEPP <cardona56.a@gmail.com>
2026-01-28 19:09:22 -0500

    Update Department_Salary_Cost_SQL_Analysis.sql

--- Department_Salary_Cost_SQL_Analysis.sql
+++ Department_Salary_Cost_SQL_Analysis.sql
@@ -1,8 +1,8 @@
 /* Total salary cost by department */
 
-SELECT COALESCE(es.dept_id, 'Unknown') AS department_id,
-	COALESCE(pd.department_name, 'Unknown') AS department_name, # Label unknowns
-    SUM(es.salary) AS department_salary_cost,
+SELECT COALESCE(es.dept_id, 'Unassigned') AS department_id,
+	COALESCE(pd.department_name, 'Unassigned') AS department_name, # Label unknowns
+    SUM(es.salary) AS total_salary,
     COUNT(es.employee_id) AS headcount, # Assuming one salary record per employee
     ROUND(AVG(es.salary)) AS avg_salary,
     ROUND(SUM(es.salary) / SUM(SUM(es.salary)) OVER() * 100,2) AS pct_of_total_payroll,
